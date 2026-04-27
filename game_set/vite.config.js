efaf26a178c5c1987c821bb2cab648167e403cc1
00chu <gamjachoi00@naver.com>
2026-04-27 11:14:15 +0900

    다시다시

--- game_set/vite.config.js
+++ game_set/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/GAME_SET_PROJECT/", // 👈 레포 이름 정확히!
+  base: "/game_set_project/",
 });
