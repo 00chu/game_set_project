1d1d7372af2c1300b5c14ac2edfa760bfdeb7bce
Choi, Jiwoo <gamjachoi00@naver.com>
2026-06-11 01:05:02 +0900

    디자인 변경

--- game_set_front/src/page/LoginPage.jsx
+++ game_set_front/src/page/LoginPage.jsx
@@ -1,31 +1,47 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-
   return (
     <main className={styles.main}>
-      <div className={styles.login_div}>
-        <p>LOGIN</p>
-        <form>
-          <ul>
-            <li>아이디</li>
-            <li>
-              <input></input>
-            </li>
-          </ul>
-          <ul>
-            <li>비밀번호</li>
-            <li>
-              <input></input>
-            </li>
-          </ul>
-          <button>LOGIN</button>
+      <div className={styles.loginCard}>
+        <div className={styles.logo}>🎮</div>
+
+        <h1>CodeArcade</h1>
+
+        <p className={styles.description}>
+          Login to save your records and rankings
+        </p>
+
+        <form className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+
+          <button type="submit" className={styles.loginBtn}>
+            LOGIN
+          </button>
         </form>
-        <div className={styles.link}>
-          <Link>회원가입</Link>
-          <Link>아이디/비밀번호 찾기</Link>
+
+        <div className={styles.links}>
+          <Link to="/signup">회원가입</Link>
+          <Link to="/find-account">아이디 / 비밀번호 찾기</Link>
+        </div>
+
+        <div className={styles.divider}>
+          <span>OR</span>
+        </div>
+
+        <div className={styles.socials}>
+          <button className={styles.google}>G</button>
+          <button className={styles.kakao}>K</button>
+          <button className={styles.apple}>A</button>
         </div>
       </div>
     </main>
