bba61dcf2821e59de576cfb786d1e43390451e3d
00chu <gamjachoi00@naver.com>
2026-06-17 11:52:53 +0900

    back 파일 추가

---
+++ game_set_back/src/main/java/com/example/game_set_back/GameSetBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_set_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSetBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSetBackApplication.class, args);
+    }
+
+}
