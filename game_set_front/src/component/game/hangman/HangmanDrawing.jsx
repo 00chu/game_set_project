d37a97acfdc11f786674c878038525546121a7eb
Choi, Jiwoo <gamjachoi00@naver.com>
2026-07-03 18:32:01 +0900

    Update HangmanDrawing.jsx

--- game_set_front/src/component/game/hangman/HangmanDrawing.jsx
+++ game_set_front/src/component/game/hangman/HangmanDrawing.jsx
@@ -1,13 +1,13 @@
 const HEAD = (
   <div
     style={{
-      width: "50px",
-      height: "50px",
-      border: "5px solid black",
-      borderRadius: "100%",
+      width: 50,
+      height: 50,
+      border: "5px solid #111",
+      borderRadius: "50%",
       position: "absolute",
-      top: "50px",
-      right: "-20px",
+      top: 50,
+      left: 95,
     }}
   />
 );
@@ -15,27 +15,12 @@ const HEAD = (
 const BODY = (
   <div
     style={{
-      width: "5px",
-      height: "100px",
-      background: "black",
+      width: 5,
+      height: 90,
+      background: "#111",
       position: "absolute",
-      top: "100px",
-      right: "0",
-    }}
-  />
-);
-
-const RIGHT_ARM = (
-  <div
-    style={{
-      width: "70px",
-      height: "5px",
-      background: "black",
-      position: "absolute",
-      top: "130px",
-      right: "-70px",
-      rotate: "-30deg",
-      transformOrigin: "left bottom",
+      top: 100,
+      left: 120,
     }}
   />
 );
@@ -43,29 +28,29 @@ const RIGHT_ARM = (
 const LEFT_ARM = (
   <div
     style={{
-      width: "70px",
-      height: "5px",
-      background: "black",
+      width: 60,
+      height: 5,
+      background: "#111",
       position: "absolute",
-      top: "130px",
-      right: "5px",
-      rotate: "30deg",
-      transformOrigin: "right bottom",
+      top: 125,
+      left: 68,
+      transform: "rotate(-30deg)",
+      transformOrigin: "right center",
     }}
   />
 );
 
-const RIGHT_LEG = (
+const RIGHT_ARM = (
   <div
     style={{
-      width: "70px",
-      height: "5px",
-      background: "black",
+      width: 60,
+      height: 5,
+      background: "#111",
       position: "absolute",
-      top: "195px",
-      right: "-60px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
+      top: 125,
+      left: 122,
+      transform: "rotate(30deg)",
+      transformOrigin: "left center",
     }}
   />
 );
@@ -73,67 +58,92 @@ const RIGHT_LEG = (
 const LEFT_LEG = (
   <div
     style={{
-      width: "70px",
-      height: "5px",
-      background: "black",
+      width: 70,
+      height: 5,
+      background: "#111",
       position: "absolute",
-      top: "195px",
-      right: "-5px",
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
+      top: 205,
+      left: 58,
+      transform: "rotate(-50deg)",
+      transformOrigin: "right center",
     }}
   />
 );
 
-const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: 70,
+      height: 5,
+      background: "#111",
+      position: "absolute",
+      top: 205,
+      left: 122,
+      transform: "rotate(50deg)",
+      transformOrigin: "left center",
+    }}
+  />
+);
+
+const BODY_PARTS = [HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG];
 
 export default function HangmanDrawing({ mistakes }) {
   return (
     <div
       style={{
         position: "relative",
-        width: "250px",
-        height: "320px",
+        width: 250,
+        height: 320,
+        margin: "0 auto",
       }}
     >
       {BODY_PARTS.slice(0, mistakes)}
 
+      {/* 밧줄 */}
       <div
         style={{
-          height: "50px",
-          width: "5px",
-          background: "black",
           position: "absolute",
           top: 0,
-          right: 0,
+          left: 120,
+          width: 4,
+          height: 50,
+          background: "#111",
         }}
       />
 
+      {/* 가로대 */}
       <div
         style={{
-          height: "5px",
-          width: "120px",
-          background: "black",
           position: "absolute",
           top: 0,
-          right: 0,
+          left: 20,
+          width: 100,
+          height: 4,
+          background: "#111",
         }}
       />
 
+      {/* 세로대 */}
       <div
         style={{
-          height: "300px",
-          width: "5px",
-          background: "black",
-          marginLeft: "120px",
+          position: "absolute",
+          top: 0,
+          left: 20,
+          width: 4,
+          height: 300,
+          background: "#111",
         }}
       />
 
+      {/* 바닥 */}
       <div
         style={{
-          height: "5px",
-          width: "200px",
-          background: "black",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: 180,
+          height: 4,
+          background: "#111",
         }}
       />
     </div>
