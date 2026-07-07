f8b59732d020e59d4012426b4af0c14e875c1f86
Choi, Jiwoo <gamjachoi00@naver.com>
2026-07-07 14:44:51 +0900

    Update vite.config.js

--- game_set_front/vite.config.js
+++ game_set_front/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/",
-});
+  // GitHub Pages용 빌드 / AWS용 빌드
+  base: mode === "github" ? "/game_set_project/" : "/",
+}));
